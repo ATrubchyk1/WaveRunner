2f1a9c86393888a71366df937dcd4f655a19e940
Andrei Trubchyk <167028688+ATrubchyk1@users.noreply.github.com>
2024-05-30 19:08:38 +0200

    Update VerticalMovingObstacle.cs

--- Assets/Scripts/Obstacles/VerticalMovingObstacle.cs
+++ Assets/Scripts/Obstacles/VerticalMovingObstacle.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 
 public class VerticalMovingObstacle : MonoBehaviour
 {
-    [SerializeField] private float _oneWayDurationTime = 1.5f;
-    [SerializeField] private Vector3 _upPosition = new Vector3(0,3,0);
-    [SerializeField] private Vector3 _downPosition = new Vector3(0,-3,0);
+    [SerializeField] private float _speed = 10;
+    [SerializeField] private float _movementDelta = 3;
+
+    private Vector3 _up;
+    private Vector3 _down;
+
     private float _currentTime;
-    
+    private bool _isMovingUp;
+    private float _oneWayTime;
+
+    private void Awake()
+    {
+        _up = new Vector3(transform.position.x , transform.position.y + _movementDelta, transform.position.z);
+        _down = new Vector3(transform.position.x, transform.position.y - _movementDelta, transform.position.z);
+        _oneWayTime = Vector3.Distance(_up, _down) / _speed;
+    }
+
     private void Update()
     {
-        _currentTime += Time.deltaTime;
-        var progress = Mathf.PingPong(_currentTime, _oneWayDurationTime) / _oneWayDurationTime;
-        transform.localPosition = Vector3.Lerp(_downPosition, _upPosition, progress);
+        _currentTime += _isMovingUp ? Time.deltaTime : -Time.deltaTime;
+        var progress = Mathf.PingPong(_currentTime, _oneWayTime) / _oneWayTime;
+        transform.position = Vector3.Lerp(_up, _down, progress);
     }
 }
